ba3fc4e16bc1e29e134353bf5f670598a6d2a5d8
Oluwafemi Sule <thezurg07@gmail.com>
2015-06-17 08:24:51 +0100

    Add user view

--- app.py
+++ app.py
@@ -39,5 +39,21 @@ def get_post(post_id):
     id = int(post_id)
     return jsonify({'post':posts[id], 'comments':comments})
 
+@app.route('/api/users', methods=['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400) # missing arguments
+    if User.query.filter_by(username = username).first() is not None:
+        abort(400) # existing user
+    user = User(username = username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({'username': user.username}), 
+                    201, 
+                    {'Location': url_for('get_user', id = user.id, _external = True)    
+
 if __name__ == '__main__':
     app.run(debug=True)
