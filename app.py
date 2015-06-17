f08a5d0eae6ced13c5243a7aa2ade225a7bca5b7
Oluwafemi Sule <thezurg07@gmail.com>
2015-06-17 08:21:55 +0100

    Use lists to mimic db

--- app.py
+++ app.py
@@ -2,5 +2,32 @@ from flask import Flask, jsonify, abort
 
 app = Flask(__name__)
 
+posts = [
+            {
+            'id': 1,
+            'title': u'Buy groceries',
+            'content': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+            'date': u'13 Jun 05',
+            'num_comment': 0,
+            'author': 1
+            },
+            {
+            'id': 2,
+            'title': u'Learn Python',
+            'content': u'Need to find a good Python tutorial on the web',
+            'date': u'14 Jun 10',
+            'num_comment': 2,
+            'author': 1
+            }
+        ]
+comments = [
+                {
+                'id': 1,
+                'content': u'Love this stuff',
+                'date': u'12 Jun 05',
+                'author': 1
+                }
+            ]
+
 if __name__ == '__main__':
     app.run(debug=True)
