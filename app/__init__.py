895755d8a88e6e8dcedb0d186ff5ec523c84a6b6
Oluwatosin Adesanya <oluwatosinadesanya@gmail.com>
2015-06-17 12:38:24 +0100

    setup database with migrations, setup flask shell

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-from app import views
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('config')
+app.config.from_pyfile('config.py')
+db = SQLAlchemy(app)
+from app import views, models
---
+++ instance/config.py
@@ -0,0 +1,4 @@
+import os
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'Sm9obiBTY2hyb20ga2lja3MgYXNz')
+print(SECRET_KEY)
