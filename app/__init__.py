db0fe48b3e0ea7a835fdd8c4fd86fc083e86979c
Oluwatosin Adesanya <oluwatosinadesanya@gmail.com>
2015-06-17 14:32:00 +0100

    setup user login/logout using openid

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,20 @@
+import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
+
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+# leave at the bottom, or else, eh?
 from app import views, models
