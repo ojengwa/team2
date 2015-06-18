50bec18c4faf10c8fcacc2c9c1f5622ef796366a
Oluwafemi Sule <thezurg07@gmail.com>
2015-06-18 17:05:53 +0100

    Create Post model child class of db.Model

--- flask_app.py
+++ flask_app.py
@@ -27,3 +27,18 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.email
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_at = db.Column(db.DateTime, default=db.func.now())
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+        self.user_id = g.user.id
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
