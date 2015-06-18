08ab0a0c9d921cfd84d9f878e6d2300297f6c946
Oluwafemi Sule <thezurg07@gmail.com>
2015-06-18 17:00:41 +0100

    Bind db to app instance with SQLAlchemy

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 basedir = os.path.dirname(os.path.abspath(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % os.path.join(basedir, 'blog.sqlite')
 app.debug = True
 app.config['WTF_CSRF_ENABLED'] = False
+
+db = SQLAlchemy(app)
