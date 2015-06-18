f5786df9ef7b33e1d860e9ad2466198c57a2b409
Oluwafemi Sule <thezurg07@gmail.com>
2015-06-18 19:25:40 +0100

    Simple request

--- client/js/blog.js
+++ client/js/blog.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
-    
+    var server = 'http://oluwafemi.pythonanywhere.com/api/v1/';
+
+    var response = $.get(server+'posts');
+
+    console.log(response);
     
 });
