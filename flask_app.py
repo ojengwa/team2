4820d6cb4a25cd7424952bf10171bde9416aa96f
Oluwafemi Sule <thezurg07@gmail.com>
2015-06-18 16:59:37 +0100

    Set Config for testing, debug=True

--- flask_app.py
+++ flask_app.py
@@ -4,3 +4,5 @@ from flask import Flask
 app = Flask(__name__)
 basedir = os.path.dirname(os.path.abspath(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % os.path.join(basedir, 'blog.sqlite')
+app.debug = True
+app.config['WTF_CSRF_ENABLED'] = False
